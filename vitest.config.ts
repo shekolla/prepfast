eeb1a432ce66a1befb40a874a814e1631a4bd162
Sai Kiran Shekolla <shekollasaikiran@gmail.com>
2026-05-09 23:50:13 +0530

    [Test] Add lcov reporter to vitest coverage config

--- vitest.config.ts
+++ vitest.config.ts
@@ -8,6 +8,7 @@ export default defineConfig({
       provider: 'v8',
       include: ['content/**/*.ts'],
       exclude: ['content/**/*.d.ts', 'content/**/data.ts'],
+      reporter: ['html', 'lcov'],
     },
   },
   resolve: {
